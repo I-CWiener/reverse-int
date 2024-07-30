6042ece30e00208a2b54a2881a212689ee884104
I-CWiener <150772092+I-CWiener@users.noreply.github.com>
2024-07-30 11:55:24 +0300

    feat: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let absNum = Math.abs(n) ;
+    let arr = (""+absNum).split("").map(Number);
+    let revArr = arr.reverse();
+    let  rezult = revArr.join('');
+    return (rezult);
 }
